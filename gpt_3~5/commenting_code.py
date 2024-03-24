4eebc206e8e9b5602beb9406272d2459c67b0f08
Akash Sengupta <52875362+akash-sgta@users.noreply.github.com>
2024-03-24 13:44:38 +0530

    phase 1

--- gpt_3~5/commenting_code.py
+++ gpt_3~5/commenting_code.py
@@ -12,7 +12,12 @@ MODEL = "gpt-3.5-turbo"
 CONTEXT_PROMPT = (
     "You are a python developer, who is proficient in Django rest framework. You have been given a "
     "task to add code comment and documentation to existing python code without disturbing the core "
-    "code or imports."
+    "code or imports. Just send back the code snippet only."
+)
+WORK_DIR = (
+    "/home/sengupta/PycharmProjects/medical-backend/backend/"
+    + "app_cdn/"
+    + "pkg_models/"
 )
 
 
@@ -29,6 +34,7 @@ def chat(obj: OpenAI, text: str):
                 "content": text,
             },
         ],
+        # max_tokens=50,
     )
     return completion.choices[0].message.content
 
@@ -38,10 +44,21 @@ if __name__ == "__main__":
         with open(os.path.join(BASE_DIR, "secret", "keys.json"), "r") as fp:
             json_data = json.loads(fp.read())
             client = OpenAI(api_key=json_data["key"])
-            response = chat(
-                obj=client,
-                text="Compose a poem that explains the concept of recursion in programming.",
-            )
-            print(response)
+            for node in os.listdir(WORK_DIR):
+                if node[:2] != "__":
+                    print(node)
+                    with open(
+                        os.path.join(WORK_DIR, node),
+                        "r",
+                    ) as file:
+                        response = chat(
+                            obj=client,
+                            text=file.read(),
+                        )
+                        with open(
+                            os.path.join(BASE_DIR, "gpt_3~5", "tmpfiles", node), "w"
+                        ) as tmp:
+                            response_lines = response.split("```")[1].split("\n")
+                            tmp.write("\n".join(response_lines[1:]))
     except Exception as e:
         print(e)
